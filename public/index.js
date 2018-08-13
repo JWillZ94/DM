6dfed09e941e28c5071fb38f17ab16ca2cada801
Jerel <jerelwilliams94@aol.com>
2018-08-13 14:59:46 -0400

    finished animations for home and about pages

---
+++ public/index.js
@@ -0,0 +1,73 @@
+"use strict;"
+
+let slideIndex = 0;
+const slides = document.getElementsByClassName("slide");
+const dots = document.getElementsByClassName("dot");
+showSlides();
+
+for (i = 0; i < dots.length; i++) {
+  dots[i].addEventListener("click", function(e) {
+    slideIndex = e.target.id;
+  });
+}
+
+function showSlides() {
+  let i;
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  dots[slideIndex].classList.add("active");
+  slides[slideIndex].style.display = "block";
+  slideIndex++;
+  if (slideIndex > slides.length - 1) slideIndex = 0;
+  setTimeout(showSlides, 3000);
+}
+
+$(document).ready(function() {
+
+  // Position of the section
+  const body2pos = $('#body-2').offset();
+  const body3pos = $('#body-3').offset();
+  const body4pos = $('#body-4').offset();
+
+  // Animation to make the content hidden
+  $('#body-2-content').animate({ opacity: 0 }, 0);
+  $('#body-3-content').animate({ opacity: 0 }, 0);
+  $('#body-4-content').animate({ opacity: 0 }, 0);
+
+  // Scroll function
+  $(window).bind('scroll', function() {
+
+    // From top
+    if ($(window).scrollTop() > body2pos.top - 300) {
+      $('#body-2-content')
+        .animate({
+          opacity: 1,
+          top: "0px"
+        }, 2000);
+    }
+
+    // From left
+    if ($(window).scrollTop() > body3pos.top - 300) {
+      $('#body-3-content')
+        .animate({
+          opacity: 1,
+          left: "0px"
+        }, 2000);
+    }
+
+    // From right
+    if ($(window).scrollTop() > body4pos.top - 300) {
+      $('#body-4-content')
+        .animate({
+          opacity: 1,
+          right: "0px"
+        }, 2000);
+    }
+
+  });
+
+});
--- public/main.js
+++ public/main.js
@@ -1,33 +1,5 @@
 "use strict;"
 
-let slideIndex = 0;
-const slides = document.getElementsByClassName("slide");
-const dots = document.getElementsByClassName("dot");
-showSlides();
-
-for (i = 0; i < dots.length; i++) {
-  dots[i].addEventListener("click", function(e) {
-    slideIndex = e.target.id;
-  });
-}
-
-function showSlides() {
-  let i;
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  dots[slideIndex].classList.add("active");
-  slides[slideIndex].style.display = "block";
-  slideIndex++;
-  if (slideIndex > slides.length - 1) slideIndex = 0;
-  setTimeout(showSlides, 3000);
-}
-
-const body2content = document.getElementById('body-2-content');
-
 // jQuery =======================================
 
 // const searchInput = document.getElementById('search-input');
@@ -41,11 +13,10 @@ $(document).ready(function() {
   //   $(searchInput).toggle('slide');
   // });
 
+  // Position of the section
   const navpos = $('.nav-section').offset();
-  const body2pos = $('#body-2').offset();
-  const body2content = document.getElementById('body-2-content');
-  $(body2content).animate({ opacity: 0 }, 0);
 
+  // Scroll function
   $(window).bind('scroll', function() {
     if ($(window).scrollTop() > navpos.top) {
       $('nav').addClass('fixed');
@@ -55,13 +26,6 @@ $(document).ready(function() {
       $('body').css("marginTop", "0px");
     }
 
-    if ($(window).scrollTop() > body2pos.top - 300) {
-      $(body2content)
-        .animate({
-          opacity: 1,
-          top: "0px"
-        }, 2000);
-    }
   });
 
   // Updates blog post
