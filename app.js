48fc1e4ed34592b82170d95873381283ac111b8f
Jerel <jerelwilliams94@aol.com>
2018-08-24 16:17:19 -0400

    added nodemailer feature, must make it secure

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const nodemailer = require('nodemailer');
 
 const app = express();
 
@@ -52,6 +53,52 @@ app.use('/blog', blog);
 
 let admin = require('./api/admin');
 app.use('/admin', admin);
+
+// NodeMailer ====================================
+
+app.post('/api/contact', (req, res) => {
+
+  const transporter = nodemailer.createTransport({
+    secure: false,
+    service: 'gmail',
+    port: 25,
+    auth: {
+      user: 'jerelwilliams94@gmail.com',
+      pass: '104280Jw'
+    },
+    tls: {
+      rejectUnauthorized: false
+    }
+  });
+
+  transporter.verify((err, success) => {
+    return err
+      ? console.log("dis a verification error: " + err)
+      : console.log("server is ready to receive messages");
+  });
+
+  const mailOptions = {
+    from: req.body.name + " " + req.body.email,
+    to: 'jerelwilliams94@gmail.com',
+    subject: 'Message from the call to action on the main site',
+    html: `
+      <p>${req.body.name} (${req.body.email}) says:</p>
+      <p>${req.body.msg}</p>
+      <p>I've heard about your business from ${req.body.source}.</p>
+      <p>My phone number is ...${req.body.phone_number}.</p>
+      <p>The best time to reach me is ${req.body.best_time}.</p>
+    `
+  };
+
+  transporter.sendMail(mailOptions, (err, info) => {
+    return err
+      ? console.log("This right here, is an error: " + err)
+      : console.log("Email sent: " + info.response);
+  });
+
+  res.redirect("/");
+
+});
 
 // Server ========================================
 
